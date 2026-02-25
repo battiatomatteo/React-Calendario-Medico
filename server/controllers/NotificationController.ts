18e8cf23d279c8a461f287c782224c93b24d2881
Battiato Matteo <99268410+battiatomatteo@users.noreply.github.com>
2026-02-25 16:02:03 +0100

    Update NotificationController.ts

--- server/controllers/NotificationController.ts
+++ server/controllers/NotificationController.ts
@@ -3,7 +3,7 @@ import { notifyOneSignal } from '../services/OneSignalServices';
 
 // Definisci il tipo del body per chiarezza
 interface NotificationBody {
-  oneSignalId: string;
+  oneSignalExternalId: string;
   subscriptionId?: string;
   onesignalIdSubscription?: string;
   titolo: string;
@@ -15,14 +15,14 @@ export async function sendNotification(
   res: Response
 ): Promise<void> {
   console.log("Richiesta ricevuta:", req.body);
-  const { oneSignalId, subscriptionId, onesignalIdSubscription, titolo, messaggio } = req.body;
+  const { oneSignalExternalId, subscriptionId, onesignalIdSubscription, titolo, messaggio } = req.body;
 
   const subId = subscriptionId || onesignalIdSubscription;
   if (!subId) {
     res.status(400).send("Missing subscriptionId");
     return;
   }
-  if (!oneSignalId) {
+  if (!oneSignalExternalId) {
     res.status(400).send("Missing oneSignalId");
     return;
   }
